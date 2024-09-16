0fc3620c258e901aa577b5ee2254acb73cb2e87b
Rebakure <kevinrebakure@gmail.com>
2024-09-16 20:25:14 +0200

    feat: configuring shadcn

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,29 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+    darkMode: ["class"],
+    content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {
-      colors: {
-        primary: {
-          100: "#FFA384",
-        },
-        secondary: {
-          100: "#74BDCB",
-        },
-        tertiary: {
-          100: "#E7F2F8",
-        },
-        accent: {
-          100: "#EFE7BC",
-        },
-      },
-    },
+  	extend: {
+  		colors: {
+  			primary: {
+  				'100': '#FFA384'
+  			},
+  			secondary: {
+  				'100': '#74BDCB'
+  			},
+  			tertiary: {
+  				'100': '#E7F2F8'
+  			},
+  			accent: {
+  				'100': '#EFE7BC'
+  			}
+  		},
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		}
+  	}
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 };
