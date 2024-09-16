186f7722b64d9fa947e00d3f014ac2fa9478fd8f
Rebakure <kevinrebakure@gmail.com>
2024-09-16 16:54:48 +0200

    chore: add custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
-    theme: {
-        extend: {},
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          100: "#FFA384",
+        },
+        secondary: {
+          100: "#74BDCB",
+        },
+        tertiary: {
+          100: "#E7F2F8",
+        },
+        accent: {
+          100: "#EFE7BC",
+        },
+      },
     },
-    plugins: [],
-}
+  },
+  plugins: [],
+};
