d14c6028081a03002d16cb9949aacd20135ae059
Rebakure <kevinrebakure@gmail.com>
2024-09-16 18:05:31 +0200

    chore: bad fix margin

--- src/components/layouts/PageLayout.tsx
+++ src/components/layouts/PageLayout.tsx
@@ -11,13 +11,18 @@ export default function PageLayout() {
         <h1 className="text-3xl font-semibold">{Titles.APP_NAME}</h1>
       )}
 
-      <Outlet />
-      <Button
-        label={ButtonLabels.ADD_ITEM}
-        iconURL={IconsURL.ADD_ICON}
-        className="font-semibold"
-      />
-      <BottomNavBar />
+      <div>
+        <Outlet />
+      </div>
+
+      <div className="px-4">
+        <Button
+          label={ButtonLabels.ADD_ITEM}
+          iconURL={IconsURL.ADD_ICON}
+          className="font-semibold"
+        />
+        <BottomNavBar />
+      </div>
     </div>
   );
 }
