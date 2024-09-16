60620be79d76754a47bb91d666e48cbbb000c7a5
Rebakure <kevinrebakure@gmail.com>
2024-09-16 18:56:20 +0200

    chore: page layout

--- src/components/layouts/PageLayout.tsx
+++ src/components/layouts/PageLayout.tsx
@@ -1,15 +1,12 @@
-import { Outlet, useLocation } from "react-router";
-import { ButtonLabels, IconsURL, RoutePaths, Titles } from "../../constants";
+import { Outlet } from "react-router";
+import { ButtonLabels, IconsURL, Titles } from "../../constants";
 import BottomNavBar from "../shared/BottomNavBar";
 import Button from "../shared/Button";
 
 export default function PageLayout() {
-  const data = useLocation();
   return (
     <div className="bg-tertiary-100 relative mx-auto min-h-screen w-[430px] space-y-10">
-      {data.pathname !== RoutePaths.INDEX && (
-        <h1 className="px-4 py-10 text-3xl font-semibold">{Titles.APP_NAME}</h1>
-      )}
+      <h1 className="px-4 py-10 text-3xl font-semibold">{Titles.APP_NAME}</h1>
 
       <div className="px-4">
         <Outlet />
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -3,19 +3,13 @@ import { Titles } from "../constants";
 
 export default function HomePage() {
   return (
-    <>
-      <div className="relative space-y-32">
-        <h1 className="bg-secondary-100 rounded-b-3xl py-10 text-center text-3xl font-semibold">
-          {Titles.APP_NAME}
-        </h1>
+    <div className="py-36">
+      <p className="text-center">{Titles.HERO}</p>
 
-        <p className="text-center">{Titles.HERO}</p>
-
-        <p className="flex items-center justify-center text-center">
-          {Titles.POWERED_BY}
-          <MongoDBIcon className="h-5 w-5" />
-        </p>
-      </div>
-    </>
+      <p className="flex items-center justify-center text-center">
+        {Titles.POWERED_BY}
+        <MongoDBIcon className="h-5 w-5" />
+      </p>
+    </div>
   );
 }
